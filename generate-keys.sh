814404f43417489a9c4510f0831c639762db6840
wiktoz <wiciazaw@wp.pl>
2025-01-08 11:48:27 +0100

    Keys generation

--- generate-keys.sh
+++ generate-keys.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+mkdir -p /app/keys
 openssl ecparam -name secp384r1 -genkey -noout -out /app/keys/ec.pem
 chmod 600 /app/keys/ec.pem
 openssl ec -in /app/keys/ec.pem -pubout -out /app/keys/ec-pub.pem
